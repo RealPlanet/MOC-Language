75f0cf54e26b48763396596ed201db3bff033092
Fabio R <fabioruscica@hotmail.it>
2021-09-29 10:41:11 +0200

    Removed unneccessary cout

--- MOCLanguage/Instruction/NumericConstant.h
+++ MOCLanguage/Instruction/NumericConstant.h
@@ -5,7 +5,6 @@ public:
 	int number;
 	NumericConstant(int data) : number { data }, Instruction(0x00) {}
 	virtual void write_bytecode(ByteBuffer* bb) const override {
-		std::cout << "Writing 32" << std::endl;
 		bb->write_byte32(number);
 	}
 };
